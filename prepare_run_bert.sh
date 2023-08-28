0842dba5c1b1795125084e1d0da917d2500ef9a2
Dawid Motyka <dawidmt@gmail.com>
2023-08-28 22:18:20 +0200

    update helper scripts

---
+++ prepare_run_bert.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp graph_features/sf_dev_dev_5000.np VAST/sf_dev_5000.np
+cp graph_features/sf_test_dev_5000.np VAST/sf_test_5000.np
+cp graph_features/sf_train_dev_5000.np VAST/sf_train5000.np
+cp VAST/sf_dev_5000.np VAST/sf_zero_dev_5000.np
+cp VAST/sf_test_5000.np VAST/sf_zero_test_5000.np
--- set_semeval_target.sh
+++ set_semeval_target.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cp datasets/$1_train.csv VAST/vast_train.csv
+cp datasets/$1_train.csv VAST/vast_train.csv
 cp datasets/$1_dev.csv VAST/vast_dev.csv
+cp datasets/$1_dev.csv VAST/vast_zero_dev.csv
 cp datasets/$1_test.csv VAST/vast_test.csv
+cp datasets/$1_test.csv VAST/vast_zero_test.csv
 
 
 
